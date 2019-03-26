7e4e17c1d2e69a24e3cc114c95b7c0b44ad89793
vishnu umakanthan <vvichugit@gmail.com>
2019-03-25 23:46:45 -0500

    initial commit

---
+++ indi.js
@@ -0,0 +1,26 @@
+function abt(){
+	window.location = "about.html";
+}
+
+function service(){
+	window.location = "services.html";
+}
+
+function navbar(){
+  
+
+
+   
+   if( document.getElementById("menu_nav").style.display == "block"){
+
+   	  document.getElementById("menu_nav").style.display = "none";
+
+   }else{
+ 
+       document.getElementById("menu_nav").style.display = "block";
+
+   }
+
+
+
+}
---
+++ services.js
@@ -0,0 +1,5 @@
+function home(){
+
+
+	window.location = "index.html";
+}
